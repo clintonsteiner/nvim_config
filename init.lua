ccda165818a20e761637198015ea7fc89252bd5f
Ben Newberg <ben.newberg@gmail.com>
2021-03-25 19:51:18 -0500

    Comment out some example lsp leader key mappings for init.lua

--- init.lua
+++ init.lua
@@ -312,15 +312,15 @@ lsp.ccls.setup {}
 lsp.pyls.setup {root_dir = lsp.util.root_pattern('.git', fn.getcwd())}
 lspfuzzy.setup {}  -- Make the LSP client use FZF instead of the quickfix list
 
-map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
-map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
-map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
-map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
-map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+-- map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+-- map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+-- map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+-- map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+-- map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+-- map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+-- map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+-- map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+-- map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
 
 -------------------- COMMANDS ------------------------------
 cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- disabled in visual mode
