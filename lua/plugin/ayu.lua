0718709c789d6d9226840c95d49ec6bc51d76660
Ben Newberg <ben.newberg@gmail.com>
2023-02-10 16:19:49 -0600

    Change sneak's highlight group to align with CurSearch

--- lua/plugin/ayu.lua
+++ lua/plugin/ayu.lua
@@ -15,7 +15,7 @@ require('ayu').setup({
         ReplaceMode = {fg = colors.markup, bg = colors.bg, reverse = true, bold = true},
         OtherMode = {fg = colors.constant, bg = colors.bg, reverse = true, bold = true},
         ScrollBar = {fg = colors.accent, bg = colors.selection_inactive},
-        Sneak = {fg = colors.bg, bg = colors.error, bold = true},
+        Sneak = {fg = colors.bg, bg = colors.special, bold = true},
         GitSignsChangeDelete = {fg = colors.constant},
         ['@variable.builtin'] = {fg = colors.constant, italic = true},
     }
