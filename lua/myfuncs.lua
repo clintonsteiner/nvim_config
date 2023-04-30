97fabc22e98a28966d4909a774bd1c415aac8fdf
Ben Newberg <ben.newberg@gmail.com>
2023-04-30 13:56:12 -0500

    Add custom git blame fzf-lua function

--- lua/myfuncs.lua
+++ lua/myfuncs.lua
@@ -28,3 +28,13 @@ function save_session()
     end
     )
 end
+
+function fzf_git_blame(opts)
+    local fzf_lua = require'fzf-lua'
+    opts = fzf_lua.defaults.git.files
+    opts.previewer = false
+    opts.prompt = "Blame> "
+    opts.cmd = "for i in " .. vim.fn.expand('%') .. " ; do git blame --color-by-age ${i} ; done"
+    opts.cwd = vim.fn.expand('%:p:h')
+    fzf_lua.fzf_exec(opts.cmd, opts)
+end
