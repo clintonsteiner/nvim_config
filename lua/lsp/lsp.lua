efeee65760288f28504fb727ad0e67e229c6cd93
Ben Newberg <ben.newberg@gmail.com>
2023-04-30 13:30:55 -0500

    Change usage of ruff lsp to be a 3rd party plugin for pylsp vs. a separate lsp

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -1,38 +1,17 @@
 local lsp = require 'lspconfig'
 lsp.pylsp.setup {
-    on_attach = function(client, bufnr)
-        client.server_capabilities.codeActionProvider = false
-        client.server_capabilities.executeCommandProvider = false
-        on_attach(client, bufnr)
-    end,
     root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),  -- start LSP server at project root or cwd
     cmd = {vim.env.HOME .. '/.virtualenvs/nvim/bin/pylsp'},
     settings = {
         pylsp = {
             plugins = {
-                flake8 = {enabled = false},
-                pycodestyle = {enabled = false},
-                pyflakes = {enabled = false},
+                ruff = {
+                    enabled = true,
+                    executable = vim.env.HOME .. '/.virtualenvs/nvim/bin/ruff',
+                    ignore = {"E501"},
+                }
             }
         }
-    },
-    handlers = {
-        ["textDocument/publishDiagnostics"] = function() end,
-    }
-}
-lsp.ruff_lsp.setup {
-    on_attach = function(client, bufnr)
-        client.server_capabilities.hoverProvider = false
-        on_attach(client, bufnr)
-    end,
-    -- root_dir = lsp.util.root_pattern('.git', vim.fn.getcwd()),
-    cmd = {vim.env.HOME .. '/.virtualenvs/nvim/bin/ruff-lsp'},
-    init_options = {
-        settings = {
-            args = {
-                "--ignore=E501",
-            },
-        },
     },
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(
