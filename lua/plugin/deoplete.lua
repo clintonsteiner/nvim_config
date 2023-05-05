433f2f9d6e9e9c6d8a2a8d39f6b543379dd8ba8a
Ben Newberg <ben.newberg@gmail.com>
2023-05-05 09:20:18 -0500

    Add icons to lsp entries in deoplete's autocompletion

--- lua/plugin/deoplete.lua
+++ lua/plugin/deoplete.lua
@@ -1,3 +1,4 @@
-vim.g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
+vim.g['deoplete#enable_at_startup'] = true
+vim.g['deoplete#lsp#use_icons_for_candidates'] = true
 vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true, noremap = true})
 vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true, noremap = true})
