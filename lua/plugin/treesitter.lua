7df63a089957f7cd2f7fc4fbc98a6790a65491ba
Ben Newberg <ben.newberg@gmail.com>
2023-06-03 10:35:43 -0500

    Add custom function to get the method/function name given the cursor position

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,2 +1,18 @@
 local ts = require 'nvim-treesitter.configs'
 ts.setup {ensure_installed = 'python', highlight = {enable = true}, indent = {enable = true}}
+
+function get_current_function_name()
+    local current_node = vim.treesitter.get_node()
+    if not current_node then return "" end
+
+    local expr = current_node
+    while expr do
+        if expr:type() == 'function_definition' then
+            break
+        end
+        expr = expr:parent()
+    end
+    if not expr then return "" end
+
+    return (vim.treesitter.get_node_text(expr:child(1), 0))
+end
