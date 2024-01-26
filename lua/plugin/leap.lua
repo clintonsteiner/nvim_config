41de4a0e368ef7c42c305a9d4bb47e0b10678f67
Ben Newberg <ben.newberg@gmail.com>
2024-01-26 12:31:55 -0600

    Add repeat mappings capability to leap to ';' and ',' behave like traditional find

--- lua/plugin/leap.lua
+++ lua/plugin/leap.lua
@@ -1,5 +1,8 @@
 local leap = require('leap')
 leap.create_default_mappings()
+leap.add_repeat_mappings(';', ',', {
+    relative_directions = true,
+})
 leap.opts.case_sensitive = true
 vim.keymap.set('o', 'z', '<Plug>(leap-forward-to)')
 vim.keymap.set('o', 'Z', '<Plug>(leap-backward-to)')
