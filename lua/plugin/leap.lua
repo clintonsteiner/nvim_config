020c8f92d9a0705d2d05edd8ed2bf696a376086d
Ben Newberg <ben.newberg@gmail.com>
2024-01-26 12:30:38 -0600

    Simplify leap setup

--- lua/plugin/leap.lua
+++ lua/plugin/leap.lua
@@ -1,4 +1,5 @@
-require('leap').create_default_mappings()
-require('leap').opts.case_sensitive = true
+local leap = require('leap')
+leap.create_default_mappings()
+leap.opts.case_sensitive = true
 vim.keymap.set('o', 'z', '<Plug>(leap-forward-to)')
 vim.keymap.set('o', 'Z', '<Plug>(leap-backward-to)')
