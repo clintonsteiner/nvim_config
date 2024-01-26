75517bc970b2d1a4748be3d481d1456d5d8f82d5
Ben Newberg <ben.newberg@gmail.com>
2024-01-26 12:08:21 -0600

    Update nvim disabled plugins to hush several :checkhealth warnings

--- lua/disable_plugins.lua
+++ lua/disable_plugins.lua
@@ -4,3 +4,7 @@ local disabled_builtins = {'gzip', 'zip', 'zipPlugin', 'tar', 'tarPlugin', 'gets
 for _, plugin in pairs(disabled_builtins) do
     vim.g["loaded_" .. plugin] = 1
 end
+
+vim.g["loaded_ruby_provider"] = 0
+vim.g["loaded_node_provider"] = 0
+vim.g["loaded_perl_provider"] = 0
