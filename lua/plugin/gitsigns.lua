6e5deebeece24649c03d74e8dac6af9d800a8487
Ben Newberg <ben.newberg@gmail.com>
2024-05-24 10:00:13 -0500

    Temporarily turn off gitsigns staged signs
    Seems buggy and hunk counts get messed up after staging occurs. Overall
    a cool feature that I hope to turn back on.

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -5,5 +5,5 @@ require('gitsigns').setup({
         changedelete = {hl = 'GitSignsChangeDelete', text = '┃'},
     },
     current_line_blame_formatter = '<abbrev_sha> <summary> <author> <author_time:%Y-%m-%d>',
-    _signs_staged_enable = true,
+    -- _signs_staged_enable = true,
 })
