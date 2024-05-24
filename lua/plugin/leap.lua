6331e2d3c33cacef67966a3c6c24935b08c1fac7
Ben Newberg <ben.newberg@gmail.com>
2024-05-24 09:47:58 -0500

    Add leap bidirectional search (normal mode only)

--- lua/plugin/leap.lua
+++ lua/plugin/leap.lua
@@ -2,7 +2,8 @@ require('leap').opts.case_sensitive = true
 require('leap.user').set_repeat_keys('<cr>', '<bs>', {
     relative_directions = true,
 })
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
-vim.keymap.set({'n', 'o'}, 'S', '<Plug>(leap-backward)')
+vim.keymap.set('n', 's', '<Plug>(leap)')
+vim.keymap.set({'x', 'o'}, 's', '<Plug>(leap-forward)')
+vim.keymap.set({'n', 'o'}, 'S', '<Plug>(leap-backward)')  -- not mapped to visual mode (x); mapping clashes
 vim.keymap.set('o', 'z', '<Plug>(leap-forward-till)')
 vim.keymap.set('o', 'Z', '<Plug>(leap-backward-till)')
