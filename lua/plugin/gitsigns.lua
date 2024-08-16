7560ce910399001dfa27cbfef985c2498382f28e
Ben Newberg <ben.newberg@gmail.com>
2024-08-16 17:10:48 -0500

    Remove deprecated gitsigns highlights (already accounted for in ayu plugin anyway)

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -2,11 +2,11 @@ require('gitsigns').setup({
     signs = {
         delete = {text = '┃'},
         -- delete = {text = '█'},  -- experiment...
-        changedelete = {hl = 'GitSignsChangeDelete', text = '┃'},
+        changedelete = {text = '┃'},
     },
     signs_staged = {
         delete = {text = '┃'},
-        changedelete = {hl = 'GitSignsChangeDelete', text = '┃'},
+        changedelete = {text = '┃'},
     },
     current_line_blame_formatter = '<abbrev_sha> <summary> <author> <author_time:%Y-%m-%d>',
     -- _signs_staged_enable = true,
