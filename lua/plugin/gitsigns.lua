f8a5b920ce78c14bf240d39039bfa3ac8ae41caa
Ben Newberg <ben.newberg@gmail.com>
2024-08-16 17:06:51 -0500

    Add signs_staged markers for gitsigns but turn the feature off (which is now default on)
    Still just incredibly buggy (navigating hunks seems to increment number
    of hunks, or it will show a staged hunk with the non-staged colors, or
    it just jumps weird).

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -4,6 +4,11 @@ require('gitsigns').setup({
         -- delete = {text = '█'},  -- experiment...
         changedelete = {hl = 'GitSignsChangeDelete', text = '┃'},
     },
+    signs_staged = {
+        delete = {text = '┃'},
+        changedelete = {hl = 'GitSignsChangeDelete', text = '┃'},
+    },
     current_line_blame_formatter = '<abbrev_sha> <summary> <author> <author_time:%Y-%m-%d>',
     -- _signs_staged_enable = true,
+    signs_staged_enable = false,  -- still way too buggy even though it's on by default
 })
